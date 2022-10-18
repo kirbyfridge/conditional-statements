d9576c016e6709bdb7ff16984c86e7ef9e549f82
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-10-18 16:20:29 -0400

    added if statements so that console will log whether expressions are true or false

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var expression1 = (x > 25);
+var expression2 = (x == 50);
 
 // Write Your JavaScript Code Here
+if(expression1 && expression2) {
+    console.log("True ✅ True ✅")
+};
+if(expression1 && !expression2) {
+    console.log("True ✅ False ❌")
+};
+if(expression2 && !expression1) {
+    console.log("False ❌ True ✅")
+};
+if(!expression1 && !expression2) {
+    console.log("False ❌ False ❌")
+}
